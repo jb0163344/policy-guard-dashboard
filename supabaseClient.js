36b85cb83cfd44c0f9a5ffa347f76b021271ed48
jb0163344 <benitezjennifer087@GMAIL.COM>
2026-02-26 16:58:15 -0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,4 +4,4 @@ const SUPABASE_URL = "https://sxvriggjkdseqahwdlws.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_ymwuEpkN-0GtSLy4WxQBaQ_fR7-wpzG";
 
-export const supabase = createClient(https://sxvriggjkdseqahwdlws.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4dnJpZ2dqa2RzZXFhaHdkbHdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2MDg3NDIsImV4cCI6MjA4MjE4NDc0Mn0.KNALnatHsOefU4UDW8QzZel8YlE_nlb4qwhGHO-xzK0);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
