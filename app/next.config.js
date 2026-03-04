de22e4af0babc0d5f131536503bdaa9a12b9f1a1
jb0163344 <benitezjennifer087@GMAIL.COM>
2026-03-04 18:09:39 -0500

    Add environment variables to next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  eslint: { ignoreDuringBuilds: true },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: "https://sxvriggjkdseqahwdlws.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: "PASTE_YOUR_PUBLISHABLE_KEY_HERE"
   },
 };
 
