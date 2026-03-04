30b0a4fb4501573d9eda2d6b1cdf0349314522cc
jb0163344 <benitezjennifer087@GMAIL.COM>
2026-03-04 18:10:40 -0500

    Refactor supabase client initialization
    Refactor supabase client initialization to avoid crashes during server-side rendering.

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// IMPORTANT: Avoid server-side build/prerender crash
+export const supabase =
+  typeof window === "undefined" || !url || !key ? null : createClient(url, key);
