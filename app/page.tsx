3ed99dd1a02985210ebdee9c6fada364ff600239
jb0163344 <benitezjennifer087@GMAIL.COM>
2026-05-25 18:40:56 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,18 @@
 
 export default function Page() {
   const test = async () => {
-    const res = await fetch("/api/risk", { method: "POST" });
+    console.log("BUTTON CLICKED");
+
+    const res = await fetch("/api/risk", {
+      method: "POST",
+    });
+
     console.log(await res.json());
   };
 
-  return <button onClick={test}>Test API</button>;
+  return (
+    <button onClick={test}>
+      Test API
+    </button>
+  );
 }
