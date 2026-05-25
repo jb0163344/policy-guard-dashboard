a047a30c43ce9f575b4b323182a0ab3291a7c531
jb0163344 <benitezjennifer087@GMAIL.COM>
2026-05-25 18:36:27 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-const res = await fetch("/api/risk", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({
-    identity_id: "test-user",
-    failedLogins: 6,
-    locationChange: true,
-    deviceUnknown: false,
-    impossibleTravel: false
-  })
-});
-
-const data = await res.json();
-alert(data.riskScore);
+fetch("/api/risk", {
+  method: "POST"
+})
