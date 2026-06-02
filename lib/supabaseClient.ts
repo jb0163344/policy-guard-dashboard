be6fae5cbe2910ee3e8a70464b54e90d55bddca9
jb0163344 <benitezjennifer087@GMAIL.COM>
2026-06-02 18:59:39 -0400

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,11 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = (() => {
-  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-  // prevent build crash
-  if (!url || !key) return null;
-
-  return createClient(url, key);
-})();
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
