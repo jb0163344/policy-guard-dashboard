508cb0a8491790c63ce07a085e20a4aef2e426ff
jb0163344 <benitezjennifer087@GMAIL.COM>
2026-06-07 14:56:08 -0400

    Update next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: "https://sxvriggjkdseqahwdlws.supabase.co",
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: "sb_publishable_ymwuEpkN-0GtSLy4WxQBaQ_fR7-wpzG"
-  },
+  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
